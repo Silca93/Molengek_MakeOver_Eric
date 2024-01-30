63eed830c091e26243d1a24612aee20ac854c2f2
Silca93 <eric_delhaye@.com>
2024-01-30 16:58:32 +0100

    main page almost finished

--- public/js/main.js
+++ public/js/main.js
@@ -29,8 +29,7 @@ logofooter();
 
 
 
-console.log(data.partenaires.bxFormation)
-
+//  
 // let carrouImage = data.partenaires.key.logo
 var slides = document.querySelectorAll('.slide');
 
@@ -49,6 +48,34 @@ Object.keys(data.partenaires).forEach((key, index) => {
     }
 });
 
+
+
+/* sceance infos */
+
+
+
+for (let index = 0; index < data.formations.seancesInfos.length; index++) {
+    let theP = document.createElement("p")
+    let infosData = data.formations.seancesInfos[index].nom;
+    theP.innerText = infosData;
+    document.querySelectorAll(".first")[index].appendChild(theP)
+    
+}
+
+
+
+
+for (let index = 0; index < data.formations.seancesInfos.length; index++) {
+        let theP = document.createElement("p")
+        let infosData = data.formations.seancesInfos[index].date;
+        theP.innerText = infosData;
+        document.querySelectorAll(".first")[index].appendChild(theP)
+        
+    }
+    
+ 
+
+   
 
 
 
